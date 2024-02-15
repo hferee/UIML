8dd76a279ec7b90fd1523a6a89b49bedcd0eadb4
Hugo Férée <feree@irif.fr>
2024-02-15 15:21:46 +0100

    fix double negation pretty printing in the classical case

--- bin/uiml_demo.ml
+++ bin/uiml_demo.ml
@@ -5,11 +5,15 @@ open Js_of_ocaml
 open Modal_expressions_parser
 open Stringconversion
 
-let rec string_of_formula ?(classical = false) = function
+let string_of_formula ?(classical = false) = 
+  let rec string_of_formula =
+  function
 | Var v -> camlstring_of_coqstring v
 | Bot -> "⊥"
 | Implies(Bot, Bot) -> "⊤"
+(* diamond *)
 | Implies(Box(Implies(f, Bot)),Bot) when classical -> "⋄ " ^ bracket f
+(* double negation *)
 | Implies(Implies(f, Bot), Bot) when classical -> string_of_formula f
 | Box f -> "□ " ^ bracket f
 | And (f, g) -> bracket f ^ " ∧ " ^ bracket g
@@ -17,35 +21,11 @@ let rec string_of_formula ?(classical = false) = function
 | Implies (f, Bot) -> "¬ " ^ bracket f (* pretty print ¬ *)
 | Implies (f, g) -> bracket f ^ " → " ^ bracket g
 and bracket e = match e with
+| Implies(Implies(f, Bot), Bot) when classical -> bracket f
+| Implies(Box(Implies(f, Bot)),Bot) when classical -> "⋄ " ^ bracket f
 | Var _ | Bot | Implies(_, Bot) -> string_of_formula e
 | e -> "(" ^ string_of_formula e ^ ")"
-(* disable optims for now
-| Implies (Bot, _) -> "⊤"                             (* ⊥ -> φ ≡ ⊤ *)
-| Implies (Implies(Bot, _), f) -> string_of_formula f (* ⊤ -> φ ≡ φ *)
-| Implies (f, Bot) -> "¬" ^ bracket f       (* φ -> ⊥ ≡ ¬ φ *)
-| Implies (_, Implies(Bot, _)) -> "⊤"                 (* φ -> ⊤ ≡ ⊤ *)
-| Implies (f, g) -> if f = g then "⊤" else            (* φ -> φ ≡ ⊤ *)
-                    bracket f ^ " → " ^ bracket g
-                    *)
-(*
-                    let var (n: int) = Var (nat_of_int n)
-let rec test_formula (n: int) =
-    if n = 0 then var 0 else
-    Implies (test_formula(n-1), var(n))
-(* let test_formula (n: int) =
-  if n = 1 then Implies(Implies(var 1, var 0), var 0) else Bot *)
-*)
-
-
-(* let example1 = "x0 | (x1 & x2)" *)
-(* let example2 = "x2 -> (x2 -> #)" *)
-
-(* let _ = print_endline (string_of_formula (simplify true (gl_UI O (eval example1))))
-let _ = print_endline (string_of_formula (simplify true (eval example2))) *)
-
-(*
-let _ = print_endline(string_of_formula (eval "□□□¬□#"))
-*)
+  in string_of_formula
 
 let catch_e f = try f() with 
   ParseError -> "Parse Error"
