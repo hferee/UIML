90ded79d541275583e664b1c8c2b05d6ee26ac8a
Hugo Férée <feree@irif.fr>
2024-06-11 11:58:17 +0200

    First step to #10.

--- bin/uiml_demo.ml
+++ bin/uiml_demo.ml
@@ -29,7 +29,15 @@ and bracket e = match e with
 
 let catch_e f = try f() with
   ParseError -> "Parse Error"
+| Failure s -> "Error: " ^ s
 | e -> "Error: " ^ Printexc.to_string e
+
+let fail_on_modality (f : form) : form =
+  let rec aux = function
+  | Box _ -> failwith "The provided formula contains modalities"
+  | Or(x, y) | Implies(x, y) | And(x, y) -> aux x ; aux y
+  | _ -> ()
+  in (aux f; f)
 
 (* export functions to js *)
 
@@ -37,15 +45,15 @@ let v : variable = coqstring_of_camlstring "p"
 let _ =
   Js.export "UIML"
     (object%js
-(* TODO: iE and iA are just an alias for isl_E and isl_A, 
-there should really at least be a function that checks that 
+(* TODO: iE and iA are just an alias for isl_E and isl_A,
+there should really at least be a function that checks that
 iE and iA are given box-free formulas as input.*)
-       method iA s =  catch_e (fun () -> string_of_formula (isl_A v (eval s)))
-       method iE s =  catch_e (fun () -> string_of_formula (isl_E v (eval s)))
-       method islA s = catch_e (fun () -> string_of_formula (isl_A v (eval s)))
-       method islE s = catch_e (fun () -> string_of_formula (isl_E v (eval s)))
-       method k s = catch_e (fun () -> string_of_formula ~classical: true (k_UI v (eval s)))
-       method gl s = catch_e (fun () -> string_of_formula ~classical: true (gl_UI v (eval s)))
-       method parse s = catch_e (fun () -> string_of_formula (eval s))
+       method iA s =  catch_e (fun () -> s |> eval |> fail_on_modality |> isl_A v |> string_of_formula)
+       method iE s =  catch_e (fun () -> s |> eval |> fail_on_modality |> isl_E v |> string_of_formula)
+       method islA s = catch_e (fun () -> s |> eval |> isl_A v |> string_of_formula)
+       method islE s = catch_e (fun () -> s |> eval |> isl_E v |> string_of_formula)
+       method k s = catch_e (fun () -> s |> eval |> isl_E v |> string_of_formula ~classical: true)
+       method gl s = catch_e (fun () -> s |> eval |> isl_E v |> string_of_formula ~classical: true)
+       method parse s = catch_e (fun () -> s |> eval |> string_of_formula)
      end);;
 
