9d6008db8ac80c2232922085049af57dbd999131
Yago Iglesias <yago.iglesias.vazquez@gmail.com>
2024-06-26 11:01:21 +0200

    benchmark computation time

--- bin/bench.ml
+++ bin/bench.ml
@@ -10,47 +10,106 @@ let rec weight = function
   | Implies (f1, f2) -> 1 + weight f1 + weight f2
   | Box f -> 1 + weight f
 
-type bench_info = { name : string; before : int; after : int }
+type 'a timed_result = { value : 'a; time : float }
 
-let percentage_reduction before after =
-  100. -. (float_of_int after /. float_of_int before *. 100.)
+type bench_info = {
+  name : string;
+  before : int timed_result;
+  after : int timed_result;
+}
 
-let print_results { name; before; after } =
-  Printf.printf "| %s | %d | %d | %.2f |\n" name before after
-    (percentage_reduction before after)
+let percentage_reduction before after = 100. -. (after /. before *. 100.)
+let percentage_increase before after = (after /. before *. 100.) -. 100.
+
+let time f x =
+  let attempts = 20 in
+  let total, value, _ =
+    List.init attempts (fun _ -> ())
+    (* Run the tests `attempts` times *)
+    |> List.map (fun _ ->
+           let t = Sys.time () in
+           let fx = f x in
+           { value = fx; time = Sys.time () -. t })
+    (* Sort them by execution time *)
+    |> List.sort (fun result1 result2 -> compare result1.time result2.time)
+    (* Compute the average time removing the slowest and fastest times *)
+    |> List.fold_left
+         (fun (total, _, len) { value; time } ->
+           if len >= 1 && len < attempts - 1 then (total +. time, value, len + 1)
+           else (total, value, len + 1))
+         (0., 0, 0)
+  in
+  { value; time = total /. float_of_int (attempts - 2) }
+
+let print_value_results { name; before; after } =
+  Printf.printf "| %s | %d | %d | %.2f |\n" name before.value after.value
+    (percentage_reduction
+       (float_of_int before.value)
+       (float_of_int after.value))
+
+let print_time_results { name; before; after } =
+  Printf.printf "| %s | %.4f | %.4f | %.2f |\n" name before.time after.time
+    (percentage_increase before.time after.time)
 
 let bench_one fs =
   let f = eval fs in
   [
     {
       name = "A " ^ fs;
-      before = weight (isl_A [ 'p' ] f);
-      after = weight (isl_simplified_A [ 'p' ] f);
+      before = time (fun f -> weight (isl_A [ 'p' ] f)) f;
+      after = time (fun f -> weight (isl_simplified_A [ 'p' ] f)) f;
     };
     {
       name = "E " ^ fs;
-      before = weight (isl_E [ 'p' ] f);
-      after = weight (isl_simplified_E [ 'p' ] f);
+      before = time (fun f -> weight (isl_E [ 'p' ] f)) f;
+      after = time (fun f -> weight (isl_simplified_E [ 'p' ] f)) f;
     };
   ]
 
+let average f l =
+  let total, len =
+    List.fold_left (fun (total, len) e -> (f e +. total, len + 1)) (0., 0) l
+  in
+  total /. float_of_int len
+
+let print_bench_value_info benches =
+  Printf.printf
+    "| Formula | Interpolant weight | Simplified interpolant weight | \
+     Percentage reduction |\n\
+     |--|--|--|--|\n";
+  List.iter print_value_results benches;
+  print_newline ();
+  Printf.printf "Average percentage reduction: %.2f\n"
+    (average
+       (fun { before; after; _ } ->
+         percentage_reduction
+           (float_of_int before.value)
+           (float_of_int after.value))
+       benches)
+
+let print_bench_time_info benches =
+  Printf.printf
+    "| Formula | Interpolant computation time (s) | Simplified interpolant \
+     computation time (s) | Percentage increase |\n\
+     |--|--|--|--|\n";
+  List.iter print_time_results benches;
+  print_newline ();
+  Printf.printf "Average percentage increase: %.2f\n"
+    (average
+       (fun { before; after; _ } -> percentage_increase before.time after.time)
+       benches);
+  Printf.printf "Average absolute time increase (s): %.5f\n"
+    (average (fun { before; after; _ } -> after.time -. before.time) benches)
+
 let bench l =
   let benches = List.map bench_one l |> List.flatten in
-  Printf.printf
-    "| Formula | Before | After | Percentage reduction |\n|--|--|--|--|\n";
-  List.iter print_results benches;
+  print_bench_value_info benches;
   print_newline ();
-  let total, len =
-    List.fold_left
-      (fun (total, len) { before; after; _ } ->
-        (percentage_reduction before after +. total, len + 1))
-      (0., 0) benches
-  in
-  Printf.printf "Average percentage reduction: %.2f\n"
-    (total /. float_of_int len)
+  print_bench_time_info benches
 
 let test_cases =
   [
+    "t ∨ q ∨ t";
     "(q -> p) & (p -> ~r)";
     "(q -> (p -> r)) -> r";
     "((q -> p) -> r) -> r";
