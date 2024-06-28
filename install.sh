c4e674a245a2b264c3572de8692c2d40b5796adc
Hugo Férée <feree@irif.fr>
2024-06-28 17:38:36 +0200

    Add exenum dependency

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 opam repo add coq-released https://coq.inria.fr/opam/released "$@"
 opam pin coq 8.19.2 "$@"
-opam install coq coq-stdpp angstrom js_of_ocaml js_of_ocaml-ppx "$@"
+opam install coq coq-stdpp angstrom js_of_ocaml js_of_ocaml-ppx exenum "$@"
