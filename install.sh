edc49c5d9e14b961f7a2896c904e009c73ec1be9
Hugo Férée <feree@irif.fr>
2025-02-08 16:36:42 +0100

    add dependency for coq-equations

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 opam repo add coq-released https://coq.inria.fr/opam/released "$@"
 opam pin coq 8.20.1 "$@"
-opam install coq coq-stdpp angstrom js_of_ocaml js_of_ocaml-ppx exenum "$@"
+opam install coq coq-stdpp coq-equations angstrom js_of_ocaml js_of_ocaml-ppx exenum "$@"
